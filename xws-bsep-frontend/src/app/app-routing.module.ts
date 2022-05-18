9de5a371a9ae976e40f12ecfaef181007dfb90f0
Kristina Stojic <stojic.kris@gmail.com>
2022-05-18 13:49:23 +0200

    initial project

---
+++ xws-bsep-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { TestComponent } from './components/test/test.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'test',
+    component: TestComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
