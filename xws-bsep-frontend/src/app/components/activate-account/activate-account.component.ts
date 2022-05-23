93b70de6416a2e6185aad47e4b839d69b38349d6
Kristina Stojic <stojic.kris@gmail.com>
2022-05-23 21:28:26 +0200

    fix: deleted successfully activated profile message when is token expired

--- xws-bsep-frontend/src/app/components/activate-account/activate-account.component.ts
+++ xws-bsep-frontend/src/app/components/activate-account/activate-account.component.ts
@@ -1,6 +1,8 @@
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-activate-account',
@@ -11,17 +13,22 @@ export class ActivateAccountComponent implements OnInit {
 
   constructor(private authService: AuthService, private route: ActivatedRoute, private router: Router) { }
   token: any
+  expired: boolean = true
+  public jwtHelper: JwtHelperService = new JwtHelperService();
 
   ngOnInit(): void {
     var url = window.location.href;
     this.token = url.split("/")[4]
     this.authService.activateAccount(this.token).subscribe(
       (token: any) => {
-      console.log(token)
+       this.expired = false
     })
+
   }
 
+
   login(){
+    //console.log(this.expired)
     this.router.navigate(['']);
 
   }
