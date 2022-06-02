f684b05e3b21fccdc48d78fa92a1f62cd7b84782
jelenahrnjak <jelenahrnjak99@gmail.com>
2022-06-02 21:23:03 +0200

    fix: fixed editing basic info

--- xws-bsep-frontend/src/app/components/update-user-modals/update-basic-info/update-basic-info.component.ts
+++ xws-bsep-frontend/src/app/components/update-user-modals/update-basic-info/update-basic-info.component.ts
@@ -5,6 +5,7 @@ import { UserService } from 'src/app/services/user.service';
 import { SuccessMessage } from 'src/app/model/success-message';
 import Swal from 'sweetalert2';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { DatePipe } from '@angular/common'; 
 
 @Component({
   selector: 'app-update-basic-info',
@@ -26,6 +27,8 @@ export class UpdateBasicInfoComponent implements OnInit {
   options: FormGroup;
   genderControl;  
   message : string = ""
+  stringBirthday : any;
+  finalBirthay : any = null;
 
   ngOnInit(): void {
     this.loadUserData(); 
@@ -39,13 +42,15 @@ export class UpdateBasicInfoComponent implements OnInit {
         (user: any) => {
         this.user = user['user']
         this.editedUser = user['user']    
-        this.genderControl = new FormControl(this.user.gender);   
+        this.genderControl = new FormControl(this.user.gender);  
+        const datepipe: DatePipe = new DatePipe('en-US')
+        this.stringBirthday = datepipe.transform(this.user.birthday, 'dd-MMMM-YYYY') 
       })
     }
   }
 
   selectBirth(){
-
+    this.finalBirthay = this.stringBirthday + "T00:00:00Z"
   }
 
   validate() : boolean{
@@ -86,10 +91,12 @@ export class UpdateBasicInfoComponent implements OnInit {
     this.editedUser.lastName = sanitize(this.user.lastName)
     this.editedUser.email = this.user.email
     this.editedUser.gender = this.user.gender
-    if(this.user.birthday && this.user.birthday != undefined){
 
-      this.editedUser.birthday = this.user.birthday + "T00:00:00Z"
-    } 
+    if(this.finalBirthay == null || !this.finalBirthay){ 
+      this.editedUser.birthday = this.user.birthday
+    } else{
+      this.editedUser.birthday = this.finalBirthay
+    }
     
     this.editedUser.gender = this.genderControl.value
 
